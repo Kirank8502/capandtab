a5e8934acba2f441b4104e1afa58ea2c240af2b7
Kiran <kirankidecha85@gmail.com>
2024-10-15 03:30:27 +0530

    jay

---
+++ erpadmin/controller/masters/pigment.php
@@ -0,0 +1,327 @@
+<?php
+namespace Opencart\Admin\Controller\Masters;
+class Pigment extends \Opencart\System\Engine\Controller {
+	private $error = array();
+	public function index():void {
+		$data = [];
+		$url = '';
+		$this->load->language('masters/pigment');
+		$this->document->setTitle('Pigment');
+		
+		$this->load->model('masters/pigment');
+
+		$data['add'] = $this->url->link('masters/pigment|form', 'user_token=' . $this->session->data['user_token'] . $url, true);
+		$data['delete'] = $this->url->link('masters/pigment|delete', 'user_token=' . $this->session->data['user_token'] . $url, true);
+		$data['list'] = $this->getList();
+		
+		$data['header'] = $this->load->controller('common/header');
+		$data['column_left'] = $this->load->controller('common/column_left');
+		$data['footer'] = $this->load->controller('common/footer');
+		
+		$data['user_token'] = $this->session->data['user_token'];
+
+		$this->response->setOutput($this->load->view('masters/pigment', $data));
+	}
+
+	public function save() {
+		$this->load->language('masters/pigment');
+		$json = [];
+		$this->document->setTitle('Pigment');
+
+		$this->load->model('masters/pigment');
+
+		if (($this->request->server['REQUEST_METHOD'] == 'POST')) {
+
+			if(!$this->request->post['pigment_id']){
+				$this->model_masters_pigment->addPigment($this->request->post);
+			}else{
+				$this->model_masters_pigment->editPigment($this->request->post['pigment_id'], $this->request->post);
+			}
+
+			$this->session->data['success'] = 'Successfully Saved';
+			$json['success'] = 'Successfully Saved';
+			$json['redirect'] = $this->url->link('masters/pigment', 'user_token=' . $this->session->data['user_token'], true);
+			
+			$this->response->addHeader('Content-Type: application/json');
+			$this->response->setOutput(json_encode($json));
+		}
+	}
+
+	public function delete() {
+		$this->load->language('masters/pigment');
+
+		$this->document->setTitle('Delete Pigment');
+
+		$this->load->model('masters/pigment');
+
+		if (isset($this->request->post['selected'])) {
+			foreach ($this->request->post['selected'] as $pigment_id) {
+				$this->model_masters_pigment->deletePigment($pigment_id);
+			}
+
+			$this->session->data['success'] = 'Successfully Deleted';
+			
+			$json['success'] = 'Successfully Deleted';
+			$json['redirect'] = $this->url->link('masters/pigment', 'user_token=' . $this->session->data['user_token'], true);
+			
+			$this->response->addHeader('Content-Type: application/json');
+			$this->response->setOutput(json_encode($json));
+		}
+
+		$this->getList();
+	}
+
+	protected function getList() {
+		if (isset($this->request->get['filter_name'])) {
+			$filter_name = $this->request->get['filter_name'];
+		} else {
+			$filter_name = '';
+		}
+
+		if (isset($this->request->get['filter_code'])) {
+			$filter_code = $this->request->get['filter_code'];
+		} else {
+			$filter_code = '';
+		}
+
+		if (isset($this->request->get['filter_status'])) {
+			$filter_status = $this->request->get['filter_status'];
+		} else {
+			$filter_status = '';
+		}
+
+		if (isset($this->request->get['sort'])) {
+			$sort = $this->request->get['sort'];
+		} else {
+			$sort = 'name';
+		}
+
+		if (isset($this->request->get['order'])) {
+			$order = $this->request->get['order'];
+		} else {
+			$order = 'ASC';
+		}
+
+		if (isset($this->request->get['page'])) {
+			$page = $this->request->get['page'];
+		} else {
+			$page = 1;
+		}
+
+		$url = '';
+
+		if (isset($this->request->get['filter_name'])) {
+			$url .= '&filter_name=' . $this->request->get['filter_name'];
+		}
+
+		if (isset($this->request->get['filter_code'])) {
+			$url .= '&filter_code=' . $this->request->get['filter_code'];
+		}
+
+		if (isset($this->request->get['filter_status'])) {
+			$url .= '&filter_status=' . $this->request->get['filter_status'];
+		}
+
+		if (isset($this->request->get['sort'])) {
+			$url .= '&sort=' . $this->request->get['sort'];
+		}
+
+		if (isset($this->request->get['order'])) {
+			$url .= '&order=' . $this->request->get['order'];
+		}
+
+		if (isset($this->request->get['page'])) {
+			$url .= '&page=' . $this->request->get['page'];
+		}
+
+		$data['breadcrumbs'] = array();
+
+		$data['breadcrumbs'][] = array(
+			'text' => $this->language->get('text_home'),
+			'href' => $this->url->link('common/dashboard', 'user_token=' . $this->session->data['user_token'], true)
+		);
+
+		$data['breadcrumbs'][] = array(
+			'text' => 'Pigment',
+			'href' => $this->url->link('masters/pigment', 'user_token=' . $this->session->data['user_token'] . $url, true)
+		);
+
+		$data['pigments'] = array();
+
+		$pigment_data = array(
+			'filter_name'  => $filter_name,
+			'filter_status'  => $filter_status,
+			'sort'  => $sort,
+			'order' => $order,
+			'start' => ($page - 1) * (int)$this->config->get('config_limit_admin'),
+			'limit' => $this->config->get('config_limit_admin')
+		);
+		
+		$pigment_total = $this->model_masters_pigment->getTotalPigments();
+		
+		$results = $this->model_masters_pigment->getPigments($pigment_data);
+		
+		foreach ($results as $result) {
+			$data['pigments'][] = array(
+				'pigment_id'		=> $result['pigment_id'],
+				'name'				=> $result['name'],
+				'qty'				=> $result['qty'],
+				'status'			=> $result['status'],
+				'selected'			=> isset($this->request->post['selected']) && in_array($result['pigment_id'], $this->request->post['selected']),				
+				'edit'				=> $this->url->link('masters/pigment|form', 'user_token=' . $this->session->data['user_token'] . '&pigment_id=' . $result['pigment_id'] . $url, true)
+			);
+		}
+		
+		if (isset($this->error['warning'])) {
+			$data['error_warning'] = $this->error['warning'];
+		} else {
+			$data['error_warning'] = '';
+		}
+
+		if (isset($this->session->data['success'])) {
+			$data['success'] = $this->session->data['success'];
+			
+			unset($this->session->data['success']);
+		} else {
+			$data['success'] = '';
+		}
+		
+		if (isset($this->request->post['selected'])) {
+			$data['selected'] = (array)$this->request->post['selected'];
+		} else {
+			$data['selected'] = array();
+		}
+
+		$url = '';
+
+		if ($order == 'ASC') {
+			$url .= '&order=DESC';
+		} else {
+			$url .= '&order=ASC';
+		}
+
+		if (isset($this->request->get['page'])) {
+			$url .= '&page=' . $this->request->get['page'];
+		}
+		
+		
+		$data['sort_name'] = $this->url->link('masters/pigment', 'user_token=' . $this->session->data['user_token'] . '&sort=name' . $url, true);
+		$data['sort_code'] = $this->url->link('masters/pigment', 'user_token=' . $this->session->data['user_token'] . '&sort=code' . $url, true);
+		$data['sort_status'] = $this->url->link('masters/pigment', 'user_token=' . $this->session->data['user_token'] . '&sort=status' . $url, true);
+		
+		$url = '';
+
+		if (isset($this->request->get['sort'])) {
+			$url .= '&sort=' . $this->request->get['sort'];
+		}
+
+		if (isset($this->request->get['order'])) {
+			$url .= '&order=' . $this->request->get['order'];
+		}
+
+		$data['pagination'] = $this->load->controller('common/pagination', [
+			'total' => $pigment_total,
+			'page'  => $page,
+			'limit' => $this->config->get('config_pagination_admin'),
+			'url'   => $this->url->link('masters/pigment|list', 'user_token=' . $this->session->data['user_token'] . $url . '&page={page}')
+		]);
+
+		$data['results'] = sprintf($this->language->get('text_pagination'), ($pigment_total) ? (($page - 1) * $this->config->get('config_pagination_admin')) + 1 : 0, ((($page - 1) * $this->config->get('config_pagination_admin')) > ($pigment_total - $this->config->get('config_pagination_admin'))) ? $pigment_total : ((($page - 1) * $this->config->get('config_pagination_admin')) + $this->config->get('config_pagination_admin')), $pigment_total, ceil($pigment_total / $this->config->get('config_pagination_admin')));
+
+		$data['filter_name'] = $filter_name;
+		$data['filter_status'] = $filter_status;
+		$data['sort'] = $sort;
+		$data['order'] = $order;
+
+		$data['user_token'] = $this->session->data['user_token'];
+
+		return $this->load->view('masters/pigment_list', $data);
+	}
+
+	public function form() {
+		$this->load->model('masters/pigment');
+		$data['text_form'] = !isset($this->request->get['pigment_id']) ? 'Add' : 'Edit';
+
+		if (isset($this->error['warning'])) {
+			$data['error_warning'] = $this->error['warning'];
+		} else {
+			$data['error_warning'] = '';
+		}
+		
+		if (isset($this->error['pigment'])) {
+			$data['error_pigment'] = $this->error['pigment'];
+		} else {
+			$data['error_pigment'] = array();
+		}
+
+		$url = '';
+
+		if (isset($this->request->get['sort'])) {
+			$url .= '&sort=' . $this->request->get['sort'];
+		}
+
+		if (isset($this->request->get['order'])) {
+			$url .= '&order=' . $this->request->get['order'];
+		}
+
+		if (isset($this->request->get['page'])) {
+			$url .= '&page=' . $this->request->get['page'];
+		}
+
+		$data['breadcrumbs'] = array();
+
+		$data['breadcrumbs'][] = array(
+			'text' => $this->language->get('text_home'),
+			'href' => $this->url->link('common/dashboard', 'user_token=' . $this->session->data['user_token'], true)
+		);
+
+		$data['breadcrumbs'][] = array(
+			'text' => 'Pigment',
+			'href' => $this->url->link('masters/pigment', 'user_token=' . $this->session->data['user_token'] . $url, true)
+		);
+
+		$data['action'] = $this->url->link('masters/pigment|save', 'user_token=' . $this->session->data['user_token'] . $url, true);
+
+		$data['back'] = $this->url->link('masters/pigment', 'user_token=' . $this->session->data['user_token'] . $url, true);
+
+		if (isset($this->request->get['pigment_id']) && ($this->request->server['REQUEST_METHOD'] != 'POST')) {
+			$pigment_info = $this->model_masters_pigment->getPigment($this->request->get['pigment_id']);
+		}
+
+		$data['user_token'] = $this->session->data['user_token'];
+
+		if (isset($this->request->post['name'])) {
+      		$data['name'] = $this->request->post['name'];
+    	} elseif (!empty($pigment_info)) {
+			$data['name'] = $pigment_info['name'];
+		} else {	
+      		$data['name'] = '';
+    	}
+
+		if(isset($this->request->get['pigment_id'])){
+			$data['pigment_id'] = $this->request->get['pigment_id'];
+		}
+		
+    	if (isset($this->request->post['status'])) {
+      		$data['status'] = $this->request->post['status'];
+    	} elseif (!empty($pigment_info)) {
+			$data['status'] = $pigment_info['status'];
+		} else {	
+      		$data['status'] = '';
+    	}
+
+		if (isset($this->request->post['qty'])) {
+			$data['qty'] = $this->request->post['qty'];
+		} elseif (!empty($pigment_info)) {
+			$data['qty'] = $pigment_info['qty'];
+	  	} else {	
+			$data['qty'] = '';
+	  	}
+		
+		$data['header'] = $this->load->controller('common/header');
+		$data['column_left'] = $this->load->controller('common/column_left');
+		$data['footer'] = $this->load->controller('common/footer');
+
+		$this->response->setOutput($this->load->view('masters/pigment_form', $data));
+	}
+}
---
+++ erpadmin/model/masters/pigment.php
@@ -0,0 +1,171 @@
+<?php
+namespace Opencart\Admin\Model\Masters;
+
+class Pigment extends \Opencart\System\Engine\Model {
+	public function addPigment($data) {
+      	$this->db->query("INSERT INTO " . DB_PREFIX . "pigment SET `name` = '" . $this->db->escape($data['pigment_name']) . "', `qty` = '" . (int)$data['pigment_qty'] . "',status = '" . (int)$data['pigment_status']."'");
+		
+		$pigment_id= $this->db->getLastId();
+		
+		$this->cache->delete('pigment');
+	}
+	
+	public function editPigment($pigment_id, $data) {
+      	$this->db->query("UPDATE " . DB_PREFIX . "pigment SET `name` = '" . $this->db->escape($data['pigment_name']) . "', `qty` = '" . (int)$data['pigment_qty'] . "', status = '" . (int)$data['pigment_status'] ."' WHERE pigment_id= '" . (int)$pigment_id. "'");
+		
+		$this->cache->delete('pigment');
+	}
+	
+	public function deletePigment($pigment_id) {
+		$this->db->query("DELETE FROM " . DB_PREFIX . "pigment WHERE pigment_id= '" . (int)$pigment_id. "'");			
+		$this->cache->delete('pigment');
+	}	
+	
+	public function getPigment($pigment_id) {
+		$query = $this->db->query("SELECT * FROM " . DB_PREFIX . "pigment WHERE pigment_id= '" . (int)$pigment_id. "'");
+		
+		return $query->row;
+	}
+	
+	public function getPigments($data = array()) {
+		$sql = "SELECT * FROM " . DB_PREFIX . "pigment";
+
+		if (!empty($data['filter_name'])) {
+			$sql .= " WHERE `name` LIKE '" . $this->db->escape($data['filter_name']) . "%'";
+		}
+
+		if (!empty($data['filter_code'])) {
+			$sql .= " WHERE `code` LIKE '" . $this->db->escape($data['filter_code']) . "%'";
+		}
+		
+		$sort_data = array(
+			'`name`',
+			'code',
+			'status'
+		);	
+		
+		if (isset($data['sort']) && in_array($data['sort'], $sort_data)) {
+			$sql .= " ORDER BY " . $data['sort'];	
+		} else {
+			$sql .= " ORDER BY `pigment_id`";	
+		}
+		
+		$sql .= " DESC";
+		
+		if (isset($data['start']) || isset($data['limit'])) {
+			if ($data['start'] < 0) {
+				$data['start'] = 0;
+			}					
+
+			if ($data['limit'] < 1) {
+				$data['limit'] = 20;
+			}	
+		
+			$sql .= " LIMIT " . (int)$data['start'] . "," . (int)$data['limit'];
+		}				
+
+		$query = $this->db->query($sql);
+	
+		return $query->rows;
+	}
+	
+	public function getTotalPigments() {
+      	$query = $this->db->query("SELECT COUNT(*) AS total FROM " . DB_PREFIX . "pigment");
+		
+		return $query->row['total'];
+	}
+	
+	public function importPigment($import_file){
+
+		$this->db->query("TRUNCATE TABLE " . DB_PREFIX . "pigment");
+		
+		$row = 0;
+		if (($handle = fopen($import_file['import_file']['tmp_name'], "r")) !== FALSE) {
+			while (($data = fgetcsv($handle, 0, ',', '"')) !== FALSE) {
+				$row++;
+				
+				$fields = array();
+				if ($row == 1) {
+//					$column_name = "`" . implode("`,`" $row) . "`";
+					continue;
+				}
+				
+				foreach ($data as $values) { $values = $this->db->escape($values); }
+				$values = implode("','", $data);
+
+				$insert_sql = "INSERT INTO " . DB_PREFIX . "pigment (type,purity,weight,price) VALUES ('{$values}');";
+				$this->db->query($insert_sql);
+				
+			}
+			fclose($handle);
+		}
+
+
+
+		//$this->db->query("load DATA LOCAL infile \"".str_replace("\\", "/", $import_file['import_file']['tmp_name'])."\" INTO TABLE " . DB_PREFIX . "assembler FIELDS TERMINATED BY '".','."' OPTIONALLY ENCLOSED BY '\"' IGNORE 1 LINES");
+	}
+
+	public function getClientDetails($data=array()) {
+
+		$sql = "SELECT SUM(o.sale_price) as sale_price, s.name as supplier_name FROM " . DB_PREFIX . "assembler s LEFT JOIN " . DB_PREFIX . "orders o ON s.supplier_id = o.supplier_id WHERE 1 ";
+
+		if (!empty($data['filter_date_from'])) {
+			$sql .= " AND DATE_FORMAT(o.order_date,'%Y-%m-%d') >= '" . $this->db->escape($data['filter_date_from']) . "'";
+		}
+
+		if (!empty($data['filter_date_to'])) {
+			$sql .= " AND DATE_FORMAT(o.order_date,'%Y-%m-%d') <= '" . $this->db->escape($data['filter_date_to']) . "'";
+		}
+
+		$sql .= " GROUP BY s.supplier_id ";
+
+		$query = $this->db->query($sql);
+
+		if($query->num_rows) {
+			return $query->rows;
+		} else {
+			return array();
+		}
+	}
+
+	public function getTotalOrders($data=array()) {
+
+		$sql = "SELECT COUNT(orders_id) as total FROM ".DB_PREFIX."orders WHERE 1 ";
+
+		if (!empty($data['filter_date_from'])) {
+			$sql .= " AND DATE_FORMAT(order_date,'%Y-%m-%d') >= '" . $this->db->escape($data['filter_date_from']) . "'";
+		}
+
+		if (!empty($data['filter_date_to'])) {
+			$sql .= " AND DATE_FORMAT(order_date,'%Y-%m-%d') <= '" . $this->db->escape($data['filter_date_to']) . "'";
+		}
+
+		$query = $this->db->query($sql);
+		if($query->num_rows) {
+			return $query->row['total'];
+		} else {
+			return 0;
+		}
+	}
+
+	public function getTotalStocks($data=array()) {
+
+		$sql = "SELECT COUNT(orders_id) as total FROM ".DB_PREFIX."orders WHERE status = 'Stock' ";
+
+		if (!empty($data['filter_date_from'])) {
+			$sql .= " AND DATE_FORMAT(order_date,'%Y-%m-%d') >= '" . $this->db->escape($data['filter_date_from']) . "'";
+		}
+
+		if (!empty($data['filter_date_to'])) {
+			$sql .= " AND DATE_FORMAT(order_date,'%Y-%m-%d') <= '" . $this->db->escape($data['filter_date_to']) . "'";
+		}
+
+		$query = $this->db->query($sql);
+		if($query->num_rows) {
+			return $query->row['total'];
+		} else {
+			return 0;
+		}
+	}
+}
+?>
